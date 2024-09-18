cc64761ee7008e135fdec048af84eb663bf7515d
YuserabanoAnsari <yuserabanoansari@dwao.in>
2024-09-18 14:17:19 +0530

    fix: fixed transparent activity blackout issue

--- example/android/app/src/main/java/com/dwao/alium_sdk_example/MainActivity.java
+++ example/android/app/src/main/java/com/dwao/alium_sdk_example/MainActivity.java
@@ -1,6 +1,14 @@
 package com.dwao.alium_sdk_example;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs;
 
 public class MainActivity extends FlutterActivity {
+  @NonNull
+  @Override
+  protected FlutterActivityLaunchConfigs.BackgroundMode getBackgroundMode() {
+    return FlutterActivityLaunchConfigs.BackgroundMode.transparent;
+  }
 }
